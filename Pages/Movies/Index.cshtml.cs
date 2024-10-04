823c15b01700c5242d6cece864ad66d100250241
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-04 12:24:48 +0100

    Lesson 3

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using dwdm_pws2_movies_app.Data;
 using dwdm_pws2_movies_app.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace dwdm_pws2_movies_app.Pages.Movies
 {
@@ -20,10 +21,27 @@ namespace dwdm_pws2_movies_app.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? Filter { get;set; }
+        public SelectList Genres { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string Genre { get;set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from f in _context.Movie orderby f.Genre select f.Genre;
+            var movies = from f in _context.Movie select f;
+
+            if (!string.IsNullOrEmpty(Filter)) {
+                movies = movies.Where(s => s.Title.Contains(Filter));
+            }
+
+            if (!string.IsNullOrEmpty(Genre)) {
+                movies = movies.Where(g => g.Genre == Genre);
+            }
+            
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
